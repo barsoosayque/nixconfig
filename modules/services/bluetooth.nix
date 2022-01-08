c35b10282fb332d0ca5602b0f6736ce84e28924f
barsoosayque <shtoshich@gmail.com>
2022-01-08 20:52:56 +0700

    trying to fix bluetooth

--- modules/services/bluetooth.nix
+++ modules/services/bluetooth.nix
@@ -12,7 +12,28 @@ in
   config = mkIf cfg.enable {
     hardware.bluetooth.enable = true;
     hardware.bluetooth.settings = {
-      General.Enable = "Source,Sink,Media,Socket";
+      General = {
+        # TODO: why ? something with pipewire
+        Enable = "Source,Sink,Media,Socket";
+
+        # https://wiki.archlinux.org/title/Bluetooth#Continually_connect/disconnect_with_tp-link_UB400_and_xbox_controller
+        JustWorksRepairing = "always";
+        FastConnectable = true;
+        Class = "0x000100";
+        Privacy = "device";
+      };
+
+      GATT = {
+        ReconnectIntervals = "1,1,2,3,5,8,13,21,34,55";
+        AutoEnable = true;
+      };
+
+      # https://github.com/atar-axis/xpadneo/issues/295
+      LE = {
+        MinConnectionInterval = 7;
+        MaxConnectionInterval = 9;
+        ConnectionLatency = 0;
+      };
     };
     services.blueman.enable = true;
   };
