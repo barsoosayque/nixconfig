03bfe61674caf39e73c6954022bb95ac5ea8e4a5
barsoosayque <shtoshich@gmail.com>
2022-01-08 20:49:08 +0700

    use workaround for steam proton

--- modules/environment/gaming.nix
+++ modules/environment/gaming.nix
@@ -16,6 +16,21 @@ in
       xpadneo  
     ];
 
+    hardware.nvidia.package = config.boot.kernelPackages.nvidia_x11;
+    
+    nixpkgs.overlays = [
+      (self: super: {
+        # https://github.com/NixOS/nixpkgs/pull/126435
+        steam = super.steam.override( {
+          extraLibraries = pkgs: [ pkgs.pipewire ];
+          extraProfile = ''
+            unset VK_ICD_FILENAMES
+            export VK_ICD_FILENAMES=${config.hardware.nvidia.package}/share/vulkan/icd.d/nvidia_icd.json:${config.hardware.nvidia.package.lib32}/share/vulkan/icd.d/nvidia_icd32.json'';
+        });
+      })
+    ];
+
+
     environment.systemPackages = [
       pkgs.logmein-hamachi
       pkgs.lutris
