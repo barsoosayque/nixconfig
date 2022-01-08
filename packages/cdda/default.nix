54c3a0a8fa98d967c0b1f19ff42c25a2f1277802
barsoosayque <shtoshich@gmail.com>
2022-01-08 20:54:21 +0700

    update flake & packages

--- packages/cdda/default.nix
+++ packages/cdda/default.nix
@@ -3,14 +3,14 @@
 with pkgs;
 stdenv.mkDerivation rec {
   pname = "cataclysmdda";
-  version = "2021-07-26-1520";
+  version = "2021-12-16-0958";
 
   buildInputs = [ SDL2 SDL2_image SDL2_mixer SDL2_ttf freetype ];
   nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
 
   src = fetchurl {
     url = "https://github.com/CleverRaven/Cataclysm-DDA/releases/download/cdda-experimental-${version}/cdda-linux-tiles-x64-${version}.tar.gz";
-    sha256 = "1d779lyqi7i5i7q8195shkgmk9sdjmggn66k0n385rb0y1kxkqgz";
+    sha256 = "dbe803786e553c80d434da3759a4825ba7c9858d17acd8216bb9004afa069777";
   };
 
   installPhase = ''
