b7a1331b134fedc72b3c6c1fc3678afc11288b56
barsoosayque <shtoshich@gmail.com>
2023-09-01 21:16:58 +0700

    update gaming module and cdda

--- modules/environment/gaming.nix
+++ modules/environment/gaming.nix
@@ -1,57 +1,120 @@
 { config, options, pkgs, lib, pkgsRepo, ... }:
 
 let
-  inherit (lib) mkIf mkEnableOption;
-  inherit (builtins) toJSON;
+  inherit (lib) types mkIf mkEnableOption mkOption lists attrsets;
+  inherit (builtins) toJSON fetchurl;
 
   cfg = config.modules.environment.gaming;
+  
+  cdda = (pkgsRepo.local.cdda.override rec {
+    typeface = [ "${pkgs.iosevka-comfy.comfy}/share/fonts/truetype/iosevka-comfy-bold.ttf" ];
+    map_typeface = [ "${pkgs.ocr-a}/share/fonts/truetype/OCRA.ttf" ];
+    overmap_typeface = map_typeface;
+
+    config_dir = "${config.system.user.dirs.games.relativePath}/cataclysmdda/";
+  });
 in
 {
   options.modules.environment.gaming = {
     enable = mkEnableOption "gaming environment";
+
+    gamepads = {
+      xbox = mkEnableOption "xbox controller";
+      nintendo = mkEnableOption "nintendo controller";
+    };
+
+    software = {
+      steam = mkEnableOption "steam";
+      lutris = mkEnableOption "lutris";
+      retroarch = mkEnableOption "retroarch";
+      wine = {
+        enable = mkEnableOption "wine";
+        package = mkOption {
+          type = with types; package;
+          default = pkgs.wine64;
+          description = "Wine package";
+        };
+      };
+    };
+
+    games = {
+      cdda = mkEnableOption "cataclysm: dark days ahead";
+      veloren = mkEnableOption "veloren";
+      minecraft = mkEnableOption "minecraft";
+    };
   };
 
   config = mkIf cfg.enable {
-    # xpadneo kernel module for xbox one controllers
-    # https://github.com/atar-axis/xpadneo
-    boot.extraModulePackages = with config.boot.kernelPackages; [
-      xpadneo
-    ];
-
-    hardware.nvidia.package = config.boot.kernelPackages.nvidia_x11;
-
-    nixpkgs.overlays = [
-      (self: super: {
+    nixpkgs.overlays = [(self: super: 
+      (attrsets.optionalAttrs cfg.software.steam {
         # https://github.com/NixOS/nixpkgs/pull/157907
-        steam = pkgsRepo.steam-fixes.steam;
-        lutris = super.lutris.override ({
-          extraPkgs = pkgs: [ pkgs.aria2 ];
+        steam = (pkgsRepo.steamFixes.steam.override {
+          extraProfile = ''
+            export VK_ICD_FILENAMES=${config.hardware.nvidia.package}/share/vulkan/icd.d/nvidia_icd.json:${config.hardware.nvidia.package.lib32}/share/vulkan/icd.d/nvidia_icd32.json:$VK_ICD_FILENAMES
+          '';
         });
-        tracker = super.tracker.overrideAttrs (_: {
-          doCheck = false;
-        });
+      }) // (attrsets.optionalAttrs cfg.software.lutris {
+        lutris = (super.lutris.override (
+          let
+            mb_glew = pkgs.glew.overrideAttrs (_: {
+              postInstall = ''ln -s $out/lib/libGLEW.so $out/lib/libGLEW.so.2.1'';
+            });
+            mb_fmod = pkgs.fmodex.overrideAttrs (prev: {
+              installPhase = prev.installPhase + ''ls $out/lib; ln -s $out/lib/libfmodex.so $out/lib/libfmodex64.so; ls $out/lib'';
+            });
+          in
+          {
+            extraPkgs = pkgs: [ pkgs.aria2 pkgs.vulkan-tools ];
+            extraLibraries = pkgs: [ mb_glew mb_fmod pkgs.curlWithGnuTls pkgs.gnome.zenity pkgs.libnice ];
+          }
+        ));
       })
-    ];
-
-    # hardware.opengl.extraPackages = [
-    #   (pkgs.runCommand "nvidia-icd" { } ''
-    #     mkdir -p $out/share/vulkan/icd.d
-    #     cp ${config.boot.kernelPackages.nvidia_x11}/share/vulkan/icd.d/nvidia_icd.x86_64.json $out/share/vulkan/icd.d/nvidia_icd.json
-    #   '')
-    # ];
+    )];
 
     environment.systemPackages = [
       pkgs.logmein-hamachi
-      pkgs.lutris
-      pkgs.steam
-    ];
+      pkgs.vulkan-tools
+    ] ++ (lists.optionals cfg.software.wine.enable [
+      cfg.software.wine.package
+      pkgs.winetricks
+    ]) ++ (lists.optional cfg.software.retroarch 
+      (pkgs.retroarch.override {
+        cores = with pkgs.libretro; [
+          # Playstation
+          swanstation # PS1 emulator  
+          ppsspp # PSP emulator
 
-    hardware.opengl = {
-      enable = true;
-      driSupport32Bit = true;
-      driSupport = true;
+          # Nintendo
+          mesen # NES emulator
+          parallel-n64 # Nintendo 64 emulator  
+          desmume # Nintendo DS emulator 
+          snes9x # SNES emulator 
+          vba-next # Gameboy advaтсe emulator
+        ];
+      })
+    ) ++ (lists.optional cfg.software.steam pkgs.steam)
+      ++ (lists.optional cfg.software.lutris pkgs.lutris)
+      ++ (lists.optional cfg.games.veloren pkgs.airshipper)
+      ++ (lists.optional cfg.games.minecraft pkgs.prismlauncher)
+      ++ (lists.optional cfg.games.cdda cdda)
+    ;
+
+    hardware = {
+      nvidia.package = config.boot.kernelPackages.nvidia_x11;
+      opengl = {
+        enable = true;
+        driSupport32Bit = true;
+        driSupport = true;
+      };
     };
+    boot.extraModulePackages = lists.optional cfg.gamepads.xbox config.boot.kernelPackages.xpadneo;
 
-    programs.steam.enable = true;
+    services.joycond.enable = cfg.gamepads.nintendo;
+    programs.steam.enable = cfg.software.steam;
+
+    system.user.hm.home.file = attrsets.optionalAttrs cfg.games.cdda {
+      "${config.system.user.dirs.games.relativePath}/cataclysmdda/game".source = "${cdda}";
+      "${config.system.user.dirs.games.relativePath}/cataclysmdda/config/fonts.json".source = "${cdda}/data/fontdata.json";
+    };
   };
 }
--- packages/cdda/default.nix
+++ packages/cdda/default.nix
@@ -1,21 +1,47 @@
-{ pkgs, ... }:
+{ pkgs
+, typeface ? [ ]
+, map_typeface ? [ ]
+, overmap_typeface ? [ ]
+, config_dir ? null
+, ...
+}:
 
 with pkgs;
+let
+  inherit (lib.strings) concatLines splitString optionalString;
+  inherit (lib.lists) last;
+  inherit (builtins) toJSON map concatStringsSep;
+
+  mkFontdataPath = font:
+    "${last (splitString "/" font)}";
+
+  fontdata = {
+    typeface = map mkFontdataPath typeface;
+    map_typeface = map mkFontdataPath map_typeface;
+    overmap_typeface = map mkFontdataPath overmap_typeface;
+  };
+
+  userdir = optionalString (! isNull config_dir) "--userdir ${config_dir}";
+in
 stdenv.mkDerivation rec {
   pname = "cataclysmdda";
-  version = "2021-12-16-0958";
+  version = "2023-03-02-0428";
 
   buildInputs = [ SDL2 SDL2_image SDL2_mixer SDL2_ttf freetype ];
   nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
 
   src = fetchurl {
-    url = "https://github.com/CleverRaven/Cataclysm-DDA/releases/download/cdda-experimental-${version}/cdda-linux-tiles-x64-${version}.tar.gz";
-    sha256 = "dbe803786e553c80d434da3759a4825ba7c9858d17acd8216bb9004afa069777";
+    url = "https://github.com/CleverRaven/Cataclysm-DDA/releases/download/cdda-experimental-${version}/cdda-linux-tiles-sounds-x64-${version}.tar.gz";
+    sha256 = "sha256-0hS10hHU1ooqqz08GJyYv0FrqfYFi/un56Ds9vOaazw=";
   };
 
   installPhase = ''
     install -m755 -D cataclysm-tiles $out/bin/cataclysm-tiles
     cp -r -t $out data gfx lang
-    wrapProgram $out/bin/cataclysm-tiles --add-flags "--basepath $out --userdir ~/games/cataclysmdda/user"
+    ${concatLines (map (f: "ln -s -f ${f} $out/data/font/") typeface)}
+    ${concatLines (map (f: "ln -s -f ${f} $out/data/font/") map_typeface)}
+    ${concatLines (map (f: "ln -s -f ${f} $out/data/font/") overmap_typeface)}
+    echo '${toJSON fontdata}' > $out/data/fontdata.json
+    wrapProgram $out/bin/cataclysm-tiles --add-flags "--basepath $out ${userdir}"
   '';
 }
