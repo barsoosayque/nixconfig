48b300bc27248ce9f27cb8c6560c0b9c9c5b4d4c
barsoosayque <shtoshich@gmail.com>
2023-09-01 21:34:11 +0700

    update station config

--- hosts/station/system.nix
+++ hosts/station/system.nix
@@ -6,19 +6,27 @@
   environment = {
     systemPackages = [
       # user
-      pkgs.spotify
       pkgs.firefox
       pkgs.discord
-      pkgs.blueberry
-
-      # games
-      pkgs.minecraft
-      pkgsRepo.local.cdda
+      pkgs.tor-browser-bundle-bin
+      pkgs.nicotine-plus
+      pkgs.yt-dlp
+      pkgs.blender
+      pkgs.signal-desktop
 
       # multimedia
       pkgs.feh
       pkgs.syncplay
       pkgs.krita
+      pkgs.tenacity
+      pkgs.ffmpeg
+      (pkgs.gifski.overrideAttrs(_: {
+        buildFeatures = ["video"];
+      }))
+      pkgs.tagger
+      # (pkgs.kdenlive.overrideAttrs (_: {
+      #   buildInputs = [ pkgs.opencv ];
+      # }))
 
       # libs
       pkgs.mono
@@ -29,7 +37,7 @@
   # TODO: droidcam module
   programs.droidcam.enable = true;
 
-  fonts.fonts = [
+  fonts.packages = [
     pkgs.iosevka-bin
     pkgs.noto-fonts
     pkgs.noto-fonts-cjk
@@ -38,7 +46,14 @@
 
   networking = {
     firewall.enable = false;
-    interfaces.enp8s0.useDHCP = true;
+    interfaces.enp8s0.ipv4.addresses = [{
+      address = "192.168.0.111";
+      prefixLength = 24;
+    }];
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    # nameservers = [ "84.200.69.80" "84.200.70.40" ]; # https://dns.watch/
+    # nameservers = [ "1.1.1.1" "1.0.0.1" ];
   };
 
   # general definitions
@@ -51,11 +66,25 @@
     };
   };
 
-  # homebrewk modules
+  # homebrew modules
   modules = {
     environment = {
       code.enable = true;
-      gaming.enable = true;
+      gaming = {
+        enable = true;
+        gamepads = {
+          xbox = true;
+        };
+        software = {
+          steam = true;
+          lutris = true;
+          wine.enable = true;
+        };
+        games = {
+          cdda = true;
+          minecraft = true;
+        };
+      };
       cli.enable = true;
       android.enable = true;
     };
@@ -65,6 +94,7 @@
       mpv.enable = true;
       scissors.enable = true;
       dmenu.enable = true;
+      obs.enable = true;
     };
 
     services = {
@@ -78,17 +108,19 @@
       transmission.enable = true;
       sound.enable = true;
       mpd.enable = true;
-      #picom.enable = true;
+      gitlabRunner.enable = true;
+      picom.enable = true;
+      polybar.enable = true;
     };
 
     x11 = {
       monitor.layout = [
         {
-          identifier = "HDMI-0";
-          resolution = { width = 1920; height = 1080; };
+          identifier = "DP-0";
+          resolution = { width = 2560; height = 1440; };
         }
         {
-          identifier = "DP-5";
+          identifier = "HDMI-0";
           resolution = { width = 1920; height = 1080; };
         }
       ];
@@ -100,8 +132,8 @@
       bspwm = {
         enable = true;
         monitors = {
-          "HDMI-0" = [ 1 2 3 4 ];
-          "DP-5" = [ 5 6 7 8 ];
+          "DP-0" = [ 1 2 3 4 ];
+          "HDMI-0" = [ 5 6 7 8 ];
         };
       };
     };
