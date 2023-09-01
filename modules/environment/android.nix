5db54d938a62050f8420d3a406b8b7e28d24c2dd
barsoosayque <shtoshich@gmail.com>
2023-09-01 21:22:44 +0700

    android studio is optional

--- modules/environment/android.nix
+++ modules/environment/android.nix
@@ -1,22 +1,26 @@
 { config, options, pkgs, lib, ... }:
 
 let
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib) mkIf mkEnableOption lists attrsets;
 
   cfg = config.modules.environment.android;
 in
 {
   options.modules.environment.android = {
     enable = mkEnableOption "android environment";
+
+    androidStudio = mkEnableOption "android studio IDE";
   };
 
   config = mkIf cfg.enable {
     environment = {
-      systemPackages = [ pkgs.android-studio ];
+      systemPackages = lists.optional cfg.androidStudio [ pkgs.android-studio ];
     };
 
     programs.adb.enable = true;
-    nixpkgs.config = {
+    users.users."${config.system.user.name}".extraGroups = ["adbusers"];
+
+    nixpkgs.config = attrsets.optionalAttrs cfg.androidStudio {
       android_sdk.accept_license = true; # Accept the Android SDK licence
     };
   };
