b6721dad41b7da337138975c71623ff360978830
barsoosayque <shtoshich@gmail.com>
2023-09-01 21:31:05 +0700

    cool blur in picom

--- modules/services/picom.nix
+++ modules/services/picom.nix
@@ -13,8 +13,32 @@ in
   config = mkIf cfg.enable {
     system.user.hm.services.picom = {
       enable = true;
-      blur = true;
-      shadow = true;
+      settings = {
+        blur = {
+          method = "dual_kawase";
+          strength = 10;
+          # kern = "7x7box";
+        };
+        blur-background-exclude = [
+          "name ?= 'slop'"
+          "class_g = 'Peek'"
+        ];
+        # shadow = true;
+
+        backend = "glx";
+        # round-borders = 1;
+        # corner-radius = 15;
+
+        unredir-if-possible = false;
+        vsync = true;
+        no-use-damage = true;
+        # xrender-sync-fence = true;
+        # vsync-use-glfinish = true;
+        # mark-wmwin-focused = true;
+        # mark-ovredir-focused = true;
+        # detect-client-opacity = true;
+        # detect-rounded-corners = true;
+      };
     };
   };
 }
