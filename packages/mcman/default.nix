2234c47f7795a5a0e7f5d1d047e0bfd9ce16c590
barsoosayque <shtoshich@gmail.com>
2024-10-28 21:34:15 +0700

    minecraft servers

--- packages/mcman/default.nix
+++ packages/mcman/default.nix
@@ -1,20 +1,29 @@
 { pkgs, ... }:
 
-# TODO: writeShellApplication
-with pkgs; stdenv.mkDerivation {
-  pname = "scissors";
-  version = "1.1";
-  buildInputs = [ maim xclip ];
-  src = ./.;
-  dontBuild = true;
-  dontStrip = true;
-  dontConfigure = true;
-  postPatch = ''
-    substituteInPlace scissors.sh \
-      --replace "maim" "${maim}/bin/maim" \
-      --replace "xclip" "${xclip}/bin/xclip"
-  '';
-  installPhase = ''
-    install -m755 -D scissors.sh $out/bin/scissors
-  '';
+with pkgs;
+rustPlatform.buildRustPackage rec {
+  pname = "mcman";
+  version = "0.4.5";
+
+  src = fetchFromGitHub {
+    owner = "ParadigmMC";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-/WIm2MFj2++QVCATDkYz2h4Jm+0RzxzVFIYrZubEgIQ=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    allowBuiltinFetchGit = true;
+    # outputHashes = {
+    #   "data-url-0.1.0" = "sha256-rESQz5jjNpVfIuTaRCAV2TLeUs09lOaLZVACsb/3Adg=";
+    #   "web_app_manifest-0.0.0" = "sha256-CpND9SxPwFmXe6fINrvd/7+HHzESh/O4GMJzaKQIjc8=";
+    #   "mime-0.4.0-a.0" = "sha256-LjM7LH6rL3moCKxVsA+RUL9lfnvY31IrqHa9pDIAZNE=";
+    # };
+  };
+
+  meta = with stdenv.lib; {
+    description = "Powerful Minecraft Server Manager CLI. Easily install jars (server, plugins & mods) and write config files.";
+    homepage = "https://github.com/${owner}/${repo}";
+  };
 }
