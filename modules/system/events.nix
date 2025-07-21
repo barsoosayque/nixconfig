5b9c08533f58a3966c7898ec1a5c55ba0d82e88e
barsoosayque <shtoshich@gmail.com>
2025-07-21 10:12:11 +0000

    remove async due to being stinky

--- modules/system/events.nix
+++ modules/system/events.nix
@@ -57,8 +57,6 @@ let
 
   mkRunScript = eventDef:
     let
-      asyncRun = "${pkgs.async}/bin/async -s=$ASYNC_SOCKET";
-
       runCallbacks = cmd:
         concatStringsSep "\n" config.system.events."${eventDef.name}Callbacks"."${cmd}";
 
@@ -71,29 +69,18 @@ let
                 export CMD_EXITCODE=$(${cmd})
                 ${runCallbacks "update"}
               '';
-
-          sh = cmd:
-            ''
-              export CMD_STDOUT="$TMP/stdout"
-              export CMD_STDERR="$TMP/stderr"
-              ${asyncRun} cmd -o $CMD_STDOUT -e $CMD_STDERR -- ${coroutine cmd}
-            '';
         in
-        concatMapStringsSep "\n" sh cmds;
+        concatMapStringsSep "\n" coroutine cmds;
 
     in
     writeScript "events-${eventDef.name}-run" ''
       #!${pkgs.dash}/bin/dash
 
       export TMP=$(mktemp -d)
-      export ASYNC_SOCKET="$TMP/socket"
       export EVENT_DESCRIPTION="${eventDef.description}"
 
       ${runCallbacks "beforeCommands"}
-      ${asyncRun} server --start
       ${runUserCmds config.system.events."${eventDef.name}"}
-      ${asyncRun} wait
-      ${asyncRun} server --stop
       ${runCallbacks "afterCommands"}
     '';
 
