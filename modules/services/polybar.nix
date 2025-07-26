0c6588e326a8e3c458daf6cb416d872a88d92348
barsoosayque <shtoshich@gmail.com>
2025-07-27 03:24:00 +0700

    Much more cooler polybar config

--- modules/services/polybar.nix
+++ modules/services/polybar.nix
@@ -9,6 +9,7 @@ let
 
   cfg = config.modules.services.polybar;
   script = ''
+    pkill polybar
     for m in $(${xrandrBin} --query | ${grepBin} " connected" | ${cutBin} -d" " -f1); do \
       MONITOR=$m polybar --reload main & \
     done
@@ -26,22 +27,26 @@ in
     system.user.hm.services.polybar = {
       enable = true;
       script = ""; # using custom events to load/reload polybar
+      package = pkgs.polybar.override {
+        pulseSupport = true;
+      };
       settings = {
         "bar/main" = {
           monitor = "\${env:MONITOR:}";
           width = "100%";
-          height = "30px";
-          bottom = true;
+          height = "40px";
+          module-margin = "15px";
+          padding = "30px";
+          # bottom = true;
           # override-redirect = false;
           fixed-center = true;
-          padding = "20px";
-          modules-left = "cpu ram";
-          modules-center = "time date";
-          modules-right = "bspwm";
-          module-margin = "20px";
+          modules-left = "keyboard time date bspwm";
+          modules-center = "";
+          modules-right = "battery backlight pulse cpu ram tray";
           wm-restack = "bspwm";
-          separator = "~";
-          font = [ "Iosevka Nerd Font:style=Medium,size=13;3" ];
+          separator = "";
+          dpi = "";
+          font = [ "${config.system.pretty.theme.fonts.bar.name}:style=Bold:size=15;3" ];
           border-color = config.system.pretty.theme.colors.bar.background.hexARGB;
           background = config.system.pretty.theme.colors.bar.background.hexARGB;
           foreground = config.system.pretty.theme.colors.bar.foreground.hexARGB;
@@ -50,11 +55,11 @@ in
         "module/bspwm" = {
           type = "internal/bspwm";
 
-          label-separator = "  ";
-          label-empty = "";
-          label-occupied  = "";
-          label-focused = "";
-          label-urgent = "";
+          label-separator = " ";
+          label-empty = " ";
+          label-occupied  = " ";
+          label-focused = " ";
+          label-urgent = "󰗖 ";
 
           label-urgent-foreground = config.system.pretty.theme.colors.bar.danger.hexARGB;
         };
@@ -73,7 +78,7 @@ in
         "module/ram" = {
           type = "internal/memory";
 
-          format = " <label>";
+          format = " <label>";
           label = "%percentage_used%%";
         };
 
@@ -105,6 +110,85 @@ in
           format = "<label>";
           date = "%d %B %Y";
           label = "%date%";
+        };
+
+        "module/battery" = {
+          type = "internal/battery";
+          full-at = 95;
+          low-at = 15;
+          battery = "BAT0";
+          adapter = "AC";
+          poll-interval = 5;
+          time-format = "%H:%M";
+
+          format-charging = "<animation-charging> <label-charging>";
+          label-charging = "%percentage%%";
+          animation-charging-0 = " ";
+          animation-charging-1 = " ";
+          animation-charging-2 = " ";
+          animation-charging-3 = " ";
+          animation-charging-4 = " ";
+          animation-charging-framerate = "750";
+
+          format-discharging = "<ramp-capacity> <label-discharging>";
+          label-discharging = "%percentage%%";
+          animation-discharging-0 = " ";
+          animation-discharging-1 = " ";
+          animation-discharging-2 = " ";
+          animation-discharging-3 = " ";
+          animation-discharging-4 = " ";
+          animation-discharging-framerate = "500";
+
+          format-full = "<ramp-capacity> <label-full>";
+          label-full = "full";
+          ramp-capacity-0 = " ";
+          ramp-capacity-1 = " ";
+          ramp-capacity-2 = " ";
+          ramp-capacity-3 = " ";
+          ramp-capacity-4 = " ";
+
+          format-low = "  <label-low> <animation-low>";
+          label-low = "%percentage%%";
+          animation-low-0 = "";
+          animation-low-1 = "";
+          animation-low-framerate = "200";
+        };
+
+        "module/backlight" = {
+          type = "internal/backlight";
+          enable-scroll = false;
+          format = "<ramp> <label>";
+          label = "%percentage%%";
+          ramp-0 = "󰛩";
+          ramp-1 = "󱩐";
+          ramp-2 = "󱩓";
+          ramp-3 = "󱩕";
+          ramp-4 = "󰛨";
+        };
+
+        "module/pulse" = {
+          type = "internal/pulseaudio";
+          use-ui-max = "true";
+          interval = 5;
+          reverse-scroll = "true";
+          click-right = "pavucontrol";
+
+          format-volume = "<ramp-volume> <label-volume>";
+          label-volume = "%percentage%%";
+          ramp-volume-0 = "";
+          ramp-volume-1 = "";
+          ramp-volume-2 = " ";
+
+          format-muted = "<label-muted>";
+          label-muted = "  off";
+          # label-muted-foreground = "#666";
+        };
+
+        "module/tray" = {
+          type = "internal/tray";
+          tray-size = "55%";
+          tray-spacing = "5px";
+          # format-margin = "8px";
         };
       };
     };
