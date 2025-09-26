8d82d716681c060aea58bed72b0825d4a9e64c70
barsoosayque <shtoshich@gmail.com>
2025-09-26 11:42:00 +0700

    can you believe it ? thermald again

--- hosts/air/system.nix
+++ hosts/air/system.nix
@@ -121,27 +121,31 @@
   services.throttled = {
     enable = true;
   };
+  services.thermald = {
+    enable = true;
+    ignoreCpuidCheck = true;
+  };
 
   # move zcfan to its own module
-  environment.etc = {
-    "zcfan.conf".text = ''
-      max_temp 85
-      med_temp 65
-      low_temp 50
-      temp_hysteresis 10
+  # environment.etc = {
+  #   "zcfan.conf".text = ''
+  #     max_temp 85
+  #     med_temp 65
+  #     low_temp 50
+  #     temp_hysteresis 10
 
-      max_level full-speed
-      med_level 4
-      low_level 1
-    '';
-  };
-  systemd.services.zcfan = {
-    description = "zcfan: A zero-configuration fan daemon for ThinkPads. ";
-    wantedBy = [ "default.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.zcfan}/bin/zcfan";
-      Type = "simple";
-    };
-  };
+  #     max_level full-speed
+  #     med_level 4
+  #     low_level 1
+  #   '';
+  # };
+  # systemd.services.zcfan = {
+  #   description = "zcfan: A zero-configuration fan daemon for ThinkPads. ";
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.zcfan}/bin/zcfan";
+  #     Type = "simple";
+  #   };
+  # };
 }
 
